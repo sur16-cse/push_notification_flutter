7b80f4ea1c5d13b218c7f7bb49de9afcea2f749b
Surbhi <surbhikumaridav@gmail.com>
2023-06-28 17:14:08 +0530

    changelog update

--- lib/utils/download_file.dart
+++ lib/utils/download_file.dart
@@ -7,9 +7,9 @@ class DownloadFile{
   static Future<String>downloadFile(String url, String fileName) async{
     final directory=await getApplicationDocumentsDirectory();
     final filePath='${directory.path}/$fileName';
+    print(filePath);
     final response =await http.get(Uri.parse(url));
     final file= File(filePath);
-
     await file.writeAsBytes(response.bodyBytes);
     return filePath;
   }
